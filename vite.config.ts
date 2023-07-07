5cf480247857b401c3e1c6ec78460ebddfdd00f2
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-07-08 01:19:56 +0900

    fix: pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -21,27 +21,27 @@ export default defineConfig({
       name: 'Better ChatGPT',
       icons: [
         {
-          src: 'better-chat-gpt/icon-192x192.png',
+          src: '/better-chat-gpt/icon-192x192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: 'better-chat-gpt/icon-256x256.png',
+          src: '/better-chat-gpt/icon-256x256.png',
           sizes: '256x256',
           type: 'image/png',
         },
         {
-          src: 'better-chat-gpt/icon-384x384.png',
+          src: '/better-chat-gpt/icon-384x384.png',
           sizes: '384x384',
           type: 'image/png',
         },
         {
-          src: 'better-chat-gpt/icon-512x512.png',
+          src: '/better-chat-gpt/icon-512x512.png',
           sizes: '512x512',
           type: 'image/png',
         },
         {
-          src: 'better-chat-gpt/icon-512x512.png',
+          src: '/better-chat-gpt/icon-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable',
