21df1bd0dd6c29fecafefeb8437a84d4806a6270
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-02-04 22:08:32 +0900

    Update function to fix bug in data processing

--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,71 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: 'auto',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-    manifest: {
-      theme_color: '#343541',
-      background_color: '#343541',
-      display: 'standalone',
-      scope: '/better-chat-gpt/',
-      start_url: './index.html',
-      short_name: 'BCGPT',
-      description: '',
-      name: 'Better ChatGPT',
-      icons: [
-        {
-          src: './icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: './icon-256x256.png',
-          sizes: '256x256',
-          type: 'image/png',
-        },
-        {
-          src: './icon-384x384.png',
-          sizes: '384x384',
-          type: 'image/png',
-        },
-        {
-          src: './icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-        {
-          src: './icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
-        },
-      ],
-    },
-  })],
-  resolve: {
-    alias: {
-      '@icon/': new URL('./src/assets/icons/', import.meta.url).pathname,
-      '@type/': new URL('./src/types/', import.meta.url).pathname,
-      '@store/': new URL('./src/store/', import.meta.url).pathname,
-      '@hooks/': new URL('./src/hooks/', import.meta.url).pathname,
-      '@constants/': new URL('./src/constants/', import.meta.url).pathname,
-      '@api/': new URL('./src/api/', import.meta.url).pathname,
-      '@components/': new URL('./src/components/', import.meta.url).pathname,
-      '@utils/': new URL('./src/utils/', import.meta.url).pathname,
-      '@src/': new URL('./src/', import.meta.url).pathname,
-    },
-  },
-  base: './',
+	plugins: [
+		react(),
+		wasm(),
+		topLevelAwait(),
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: "auto",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+			manifest: {
+				theme_color: "#343541",
+				background_color: "#343541",
+				display: "standalone",
+				scope: "/better-chat-gpt/",
+				start_url: "./index.html",
+				short_name: "BCGPT",
+				description: "",
+				name: "Better ChatGPT",
+				icons: [
+					{
+						src: "./icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "./icon-256x256.png",
+						sizes: "256x256",
+						type: "image/png",
+					},
+					{
+						src: "./icon-384x384.png",
+						sizes: "384x384",
+						type: "image/png",
+					},
+					{
+						src: "./icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "./icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@icon/": new URL("./src/assets/icons/", import.meta.url).pathname,
+			"@type/": new URL("./src/types/", import.meta.url).pathname,
+			"@store/": new URL("./src/store/", import.meta.url).pathname,
+			"@hooks/": new URL("./src/hooks/", import.meta.url).pathname,
+			"@constants/": new URL("./src/constants/", import.meta.url).pathname,
+			"@api/": new URL("./src/api/", import.meta.url).pathname,
+			"@components/": new URL("./src/components/", import.meta.url).pathname,
+			"@utils/": new URL("./src/utils/", import.meta.url).pathname,
+			"@src/": new URL("./src/", import.meta.url).pathname,
+		},
+	},
+	base: "./",
 });
